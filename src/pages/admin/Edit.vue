aca2bb64cbdaebb7e4a62e252bc00a439eede9a5
pftom <1043269994@qq.com>
2019-10-14 10:59:38 +0800

    使用嵌套路由和动态路由合理组织页面

---
+++ src/pages/admin/Edit.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    <div class="title">
+      <h1>This is Admin/Edit/{{$route.params.id}}</h1>
+    </div>
+  </div>
+</template>
---
+++ src/pages/admin/Index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <div class="admin-new">
+      <div class="container">
+        <div class="col-lg-3 col-md-3 col-sm-12 col-xs-12">
+          <ul class="admin-menu">
+            <li>
+              <router-link to="/admin">View Products</router-link>
+            </li>
+            <li>
+              <router-link to="/admin/new">New Products</router-link>
+            </li>
+          </ul>
+        </div>
+        <router-view></router-view>
+      </div>
+    </div>
+  </div>
+</template>
