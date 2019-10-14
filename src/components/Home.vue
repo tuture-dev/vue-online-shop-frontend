056d55500fa1ad1552d70a41094169ffbc0617d0
pftom <1043269994@qq.com>
2019-10-14 10:37:54 +0800

    编写你的第一个 Vue 页面

---
+++ src/components/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <div class="title">
+      <h1>{{msg}}</h1>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'home',
+    data () {
+      return {
+        msg: 'Welcome to Your Vue.js App'
+      };
+    },
+  }
+</script>
