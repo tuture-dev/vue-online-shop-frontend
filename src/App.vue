19a30e79092da3c920fc7904eee08efacfda1b97
pftom <1043269994@qq.com>
2019-10-14 10:46:16 +0800

    使用路由进行多页面跳转

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,21 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <nav>
+      <div class="container">
+        <ul class="nav__left">
+          <li>
+            <router-link to="/">Home</router-link>
+          </li>
+          <li>
+            <router-link to="/admin">Admin</router-link>
+          </li>
+          <li>
+            <router-link to="/cart">Cart</router-link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+
     <router-view/>
   </div>
 </template>
