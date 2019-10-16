b14d923012e6160d8257804393c351d1b77b9bde
pftom <1043269994@qq.com>
2019-10-16 23:14:17 +0800

    使用 Action 获取远程数据

--- src/components/products/ProductList.vue
+++ src/components/products/ProductList.vue
@@ -9,7 +9,7 @@
           <p class="product__name">产品名称：{{product.name}}</p>
           <p class="product__description">介绍：{{product.description}}</p>
           <p class="product__price">价格：{{product.price}}</p>
-          <p class="product.manufacturer">生产厂商：{{product.manufacturer}}</p>
+          <p class="product.manufacturer">生产厂商：{{product.manufacturer.name}}</p>
           <img :src="product.image" alt="" class="product__image">
           <button @click="addToCart(product)">加入购物车</button>
         </div>
@@ -32,6 +32,11 @@
 <script>
 export default {
   name: 'product-list',
+  created() {
+    if (this.products.length === 0) {
+      this.$store.dispatch('allProducts')
+    }
+  },
   computed: {
     // a computed getter
     products() {
--- src/pages/Cart.vue
+++ src/pages/Cart.vue
@@ -8,7 +8,7 @@
         <p class="product__name">产品名称：{{product.name}}</p>
         <p class="product__description">介绍：{{product.description}}</p>
         <p class="product__price">价格：{{product.price}}</p>
-        <p class="product.manufacturer">生产厂商：{{product.manufacturer}}</p>
+        <p class="product.manufacturer">生产厂商：{{product.manufacturer.name}}</p>
         <img :src="product.image" alt="" class="product__image">
         <button @click="removeFromCart(product._id)">从购物车中移除</button>
       </div>
