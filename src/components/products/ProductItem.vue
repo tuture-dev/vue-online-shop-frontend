9ed8f6ab2fde5d83ccaeaf7272f495ab222fab22
pftom <1043269994@qq.com>
2019-10-17 10:02:20 +0800

    使用 Vue 组件简化页面逻辑

---
+++ src/components/products/ProductItem.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <div class="product">
+      <p class="product__name">产品名称：{{product.name}}</p>
+      <p class="product__description">介绍：{{product.description}}</p>
+      <p class="product__price">价格：{{product.price}}</p>
+      <p class="product.manufacturer">生产厂商：{{product.manufacturer.name}}</p>
+      <img :src="product.image" alt="" class="product__image">
+      <product-button :product="product"></product-button>
+    </div>
+  </div>
+</template>
+
+<script>
+import ProductButton from './ProductButton';
+export default {
+  name: 'product-item',
+  props: ['product'],
+  components: {
+    'product-button': ProductButton,
+  }
+}
+</script>
