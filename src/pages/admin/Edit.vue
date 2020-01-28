4125181fd926026864fc3cff5407628c7cc5ca36
pftom <1043269994@qq.com>
2020-01-28 14:43:51 +0800

    修复 ElementUI 表单的双向绑定问题

--- src/pages/admin/Edit.vue
+++ src/pages/admin/Edit.vue
@@ -8,46 +8,46 @@
       :model="model"
       :manufacturers="manufacturers"
       :isEditing="true"
-      ></product-form>
+    ></product-form>
   </div>
 </template>
 
 <script>
-import ProductForm from '@/components/products/ProductForm.vue';
+import ProductForm from "@/components/products/ProductForm.vue";
 export default {
-  data: {
-    model() {
-      const product = this.$store.getters.productById(this.$route.params['id']);
-      // 这里返回 product 的拷贝，是为了在修改 product 的拷贝之后，在保存之前不修改本地 Vuex stire 的 product 属性
-      return { ...product, manufacturer: { ...product.manufacturer } };
-    }
-  },
   created() {
-    const { name } = this.model;
+    const { name = "" } = this.modelData || {};
+
     if (!name) {
-      this.$store.dispatch('productById', {
-        productId: this.$route.params['id']
+      this.$store.dispatch("productById", {
+        productId: this.$route.params["id"]
       });
     }
 
     if (this.manufacturers.length === 0) {
-      this.$store.dispatch('allManufacturers');
+      this.$store.dispatch("allManufacturers");
     }
   },
   computed: {
     manufacturers() {
       return this.$store.getters.allManufacturers;
+    },
+    model() {
+      const product = this.$store.getters.productById(this.$route.params["id"]);
+      const res = { ...product, manufacturer: { ...product.manufacturer } };
+
+      return res;
     }
   },
   methods: {
     updateProduct(product) {
-      this.$store.dispatch('updateProduct', {
-        product,
-      })
+      this.$store.dispatch("updateProduct", {
+        product
+      });
     }
   },
   components: {
-    'product-form': ProductForm
+    "product-form": ProductForm
   }
-}
+};
 </script>
