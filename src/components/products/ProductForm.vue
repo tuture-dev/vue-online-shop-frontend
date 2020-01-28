02f5761fdc540a3cf3e0549b2a4e3f616b4f1a48
pftom <1043269994@qq.com>
2020-01-28 14:43:51 +0800

    修复 ElementUI 表单的双向绑定问题

--- src/components/products/ProductForm.vue
+++ src/components/products/ProductForm.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="productInfo">
-    <el-form class="form" ref="form" :model="model" label-width="180px">
+    <el-form class="form" ref="form" label-width="180px">
       <el-form-item label="Name">
         <el-input v-model="model.name"></el-input>
       </el-form-item>
@@ -8,12 +8,17 @@
         <el-input v-model="model.price"></el-input>
       </el-form-item>
       <el-form-item label="Manufacturer ">
-        <el-select v-model="model.manufacturer.name" clearable placeholder="请选择制造商">
+        <el-select
+          v-model="modelData.manufacturer.name"
+          clearable
+          placeholder="请选择制造商"
+        >
           <el-option
             v-for="manufacturer in manufacturers"
             :key="manufacturer._id"
             :label="manufacturer.name"
-            :value="manufacturer.name">
+            :value="manufacturer.name"
+          >
           </el-option>
         </el-select>
       </el-form-item>
@@ -24,7 +29,9 @@
         <el-input type="textarea" v-model="model.description"></el-input>
       </el-form-item>
       <el-form-item>
-        <el-button v-if="isEditing" type="primary" @click="onSubmit">Update Product</el-button>
+        <el-button v-if="isEditing" type="primary" @click="onSubmit"
+          >Update Product</el-button
+        >
         <el-button v-else @click="onSubmit">Add Product</el-button>
       </el-form-item>
     </el-form>
@@ -63,7 +70,7 @@
       </div>
     </div> -->
 
-    <!-- <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
+  <!-- <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
       <div class="form-group">
         <label>Image</label>
         <input
@@ -87,8 +94,8 @@
       <div class="form-group new-button">
         <button class="button">
           <i class="fa fa-pencil"></i> -->
-          <!-- Conditional rendering for input text -->
-          <!-- <span v-if="isEditing">Update Product</span>
+  <!-- Conditional rendering for input text -->
+  <!-- <span v-if="isEditing">Update Product</span>
           <span v-else>Add Product</span>
         </button>
       </div>
@@ -98,16 +105,28 @@
 
 <script>
 export default {
-  props: ['model', 'manufacturers', 'isEditing'],
+  data() {
+    return {
+      modelData: { manufacturer: { name: "" } }
+    };
+  },
+  props: ["model", "manufacturers", "isEditing"],
   created() {
-    console.log(this.model)
+    const product = this.model;
+
+    this.modelData = { ...product, manufacturer: { ...product.manufacturer } };
+  },
+  watch: {
+    model(val, oldVal) {
+      this.modelData = val;
+    }
   },
   methods: {
     onSubmit() {
-      this.$emit('save-product', this.model)
+      this.$emit("save-product", this.modelData);
     }
   }
-}
+};
 </script>
 <style>
 .productInfo {
