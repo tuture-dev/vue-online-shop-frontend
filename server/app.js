cc4d59c976050064882d1e08f448c4525d35b2dc
mRcfps <1402491442@qq.com>
2020-03-14 20:10:12 +0800

    配置 MongoDB 的身份验证

--- server/app.js
+++ server/app.js
@@ -16,7 +16,12 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 // Datbase connection here
-mongoose.connect(process.env.MONGO_URI || `mongodb://localhost:27017/test`);
+mongoose.connect(process.env.MONGO_URI || `mongodb://localhost:27017/test`, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  user: process.env.MONGO_USER,
+  pass: process.env.MONGO_PASSWORD,
+});
 
 // CORS config here
 app.all('/*', function(req, res, next) {
